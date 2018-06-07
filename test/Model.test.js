cde44e2614ad96c990b00283d3ae93aa5dbeb495
Samir Patel <samirpatelgx@gmail.com>
2018-06-07 19:57:14 -0400

    Added deleteOne functionality

--- test/Model.test.js
+++ test/Model.test.js
@@ -1,7 +1,10 @@
 const dynoMongo = require("../");
 const dynoUtils = require("../lib/DynoUtils");
 const testObjects = require("./testObjects")
+const uuidv1 = require("uuid/v1");
 const { Schema } = dynoMongo;
+const delay = (ms) => { return new Promise(resolve => setTimeout(resolve,ms)) };
+
 dynoMongo.AWS.config.update({
   accessKeyId: 'AKID',
   secretAccessKey: 'SECRET',
@@ -9,41 +12,50 @@ dynoMongo.AWS.config.update({
 });
 
 dynoMongo.local();
-var personSchema, personTable, Person, newPerson, keySchema, newSchema
-newSchema = dynoUtils.createDynamoTree(personSchema);
-const delay = (ms) => { return new Promise(resolve => setTimeout(resolve,ms)) };
+var personSchema, personTable, Person, newPerson, keySchema, newSchema, tableName
+
+personSchema = new Schema({
+  peopleId: {
+    type: "string",
+    hashKey: true
+  },
+  count: {
+    type: "number",
+    rangeKey: true
+  },
+  throughput: 2
+})
+
 
 describe("Model test", () => {
 
-  
-  personSchema = new Schema({
-    peopleId: {
-      type: "string",
-      hashKey: true
-    },
-    count: {
-      type: "number",
-      rangeKey: true
-    },
-    throughput: 2
-  })
 
   
-  beforeEach(() => {
-    personTable = dynoMongo.model("person_test_schema_one", personSchema);
-    Person = dynoMongo.model("person_test_schema_one");
+  beforeEach(async () => {
+    tableName = "test";
+    await delay(50);
+
+    newSchema = dynoUtils.createDynamoTree(personSchema);
+    personTable = dynoMongo.model(tableName, personSchema);
+    Person = dynoMongo.model(tableName);
     newPerson = new Person(testObjects.personModel.people_one)
+    
   });
   afterEach(async () => {
     await delay(50);
-    dynoMongo.ddb.deleteTable({
-      TableName: personTable.table.TableName
-    }, (err, res) => {
-      if (err) console.debug(err);
-      console.debug(res)
-    })
+    // dynoMongo.ddb.deleteTable({
+    //   TableName: personTable.table.TableName
+    // }, (err, res) => {
+    //   if (err) console.debug(err);
+    //   console.debug(res)
+    // })
+    newSchema = null;
+    personTable = null;
+    Person = null;
+    newPerson = null;
+    delete dynoMongo.blueprints[tableName];
   })
-  describe("Save to database", () => {
+  describe("Put Item", () => {
     beforeEach(() => {
       keySchema = dynoUtils.getKeySchema(newPerson); // Here is what needs fixing - Need to create function that only works off Table.js and not Model.js
     })
@@ -54,34 +66,36 @@ describe("Model test", () => {
         Key : keySchema
       })
     })
-    it ("It should save person data to the database", async () => {
+    it ("It should add an item to the database", async () => {
+      let res = "error";
       try {
         await delay(50);
-        let res = await newPerson.save()
-        console.debug(res);
+        res = await newPerson.save()
       }
       catch(err) {
         console.debug(err);
       };
+      expect(res).toEqual({});
     });
   });
-  describe("Delete an item from the database", () => {
+  describe("Delete Item", () => {
     beforeEach(() => {
       keySchema = dynoUtils.getKeySchema(newPerson);
       dynoMongo.ddb.putItem({
         TableName: personTable.table.TableName,
-        Key : keySchema
+        Key : newSchema
       })
     })
-    it ("It should save person data to the database", async () => {
+    it ("It should delete an item from the database", async () => {
+      let res = "error";
       try {
         await delay(50);
-        let res = await newPerson.save()
-        console.debug(res);
+        res = await newPerson.deleteOne(keySchema);
       }
       catch(err) {
         console.debug(err);
       };
+      expect(res).toEqual({});
     });
   });
 });
